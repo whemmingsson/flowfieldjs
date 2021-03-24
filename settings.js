cbaca7c7863c4c6f6bd2f6d220bc7be3f1feafb0
William Hemmingsson <william.hemmingsson@storytel.com>
2021-03-24 11:06:22 +0100

    More work on settings

--- settings.js
+++ settings.js
@@ -1,31 +1,21 @@
 let Settings = {
-    NUMBER_OF_PARTICLES: 10,
-    FIELD_SCALE : 40,
-    FORCE_STRENGTH: 0.6,
-    OFFSET_SPEED: 0.08,
-    TIME_OFFSET_SPEED: 0.01,
-    DETAIL: 15,
+  NUMBER_OF_PARTICLES: 10,
+  FIELD_SCALE: 40,
+  FORCE_STRENGTH: 0.6,
+  OFFSET_SPEED: 0.08,
+  TIME_OFFSET_SPEED: 0.01,
+  DETAIL: 15,
 
+  MAX_SPEED: 5,
+  THICKNESS: 5,
+  ALPHA: 255,
 
-    MAX_SPEED: 5,
-    THICKNESS: 5,
-    ALPHA: 255,
+  USE_RAINBOW: true,
+  DRAW_VECTORS: false,
+  DRAW_PARTICLES: true,
+  DRAW_FPS: true,
 
-    
-    Field: {
-        ENABLE_TIME: true,
-
-    },
-    Particle: {
-        MAX_SPEED: 5,
-        THICKNESS: 5,
-        ALPHA: 255,
-    },
-    Env: {
-        
-        USE_RAINBOW: false,
-        DRAW_VECTORS: true,
-        DRAW_PARTICLES: true,
-        DRAW_FPS: true,
-    }
+  Field: {
+    ENABLE_TIME: true,
+  },
 };
